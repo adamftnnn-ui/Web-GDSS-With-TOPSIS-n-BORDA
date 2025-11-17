6887bf9c6dda6e8cfd52aa7ef5772f5e95751c0a
Faiq Rafii <rafiiwahyudi25@gmail.com>
2025-11-17 21:00:28 +0700

    update mongoose connect error

--- backend/server.js
+++ backend/server.js
@@ -11,22 +11,28 @@ const penilaianRouter = require("./routes/penilaian");
 const { authenticateToken } = require("./controllers/authController");
 
 dotenv.config();
+
+const MONGO_URL = process.env.MONGO_URL;
+// mongoose
+//   .connect(MONGO_URL, {
+//     serverSelectionTimeoutMS: 30000,
+//   })
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//   })
+//   .catch((e) => {
+//     console.error("Failed to connect to MongoDB", e);
+//   });
+mongoose.connect(MONGO_URL)
+mongoose.Promise=global.Promise
+mongoose.connection.on('error',(err)=>{
+  console.error(`Errror connecting to mongodb: ${err.message}`);
+})
+
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
-
-const MONGO_URL = process.env.MONGO_URL;
-mongoose
-  .connect(MONGO_URL, {
-    serverSelectionTimeoutMS: 30000,
-  })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((e) => {
-    console.error("Failed to connect to MongoDB", e);
-  });
 
 const port = process.env.PORT;
 app.listen(port, () => {
